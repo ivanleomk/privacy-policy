5101d3720e3590875b5606167a6c718af48a660e
Ivan Leo <ivanleomk@gmail.com>
2021-03-15 00:05:57 +0800

    Fixed up privacy policy page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,104 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+    <>
+      <div class="relative py-16 bg-white overflow-hidden">
+        <div class="relative px-4 sm:px-6 lg:px-8">
+          <div class="text-lg max-w-prose mx-auto">
+            <h1>
+              <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                Unboxed.sg
+              </span>
+              <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+                Our Privacy Policy - (Last updated: 14/3/2021)
+              </span>
+            </h1>
+          </div>
+          <div class="mt-6 prose prose-indigo prose-lg text-justify text-gray-500 mx-auto">
+            <h2>Ownership</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Unboxed operates unboxed.sg facebook page.This page informs you of
+              our policies regarding the collection, use and disclosure of
+              personal information we receive from users of the site. We use
+              your personal information only for providing and improving the
+              site.By using the site, you agree to the collection and use of
+              information in accordance with this policy.
             </p>
-          </a>
+            <h2>Information Collection And Use</h2>
+            <p>
+              While using our Site, we may ask you to provide us with certain
+              personally identifiable information that can be used to contact or
+              identify you. Personally identifiable information may include, but
+              is not limited to your name ("Personal Information"). Log Data
+              Like many site operators, we collect information that your browser
+              sends whenever you visit our Site ("Log Data"). This Log Data may
+              include information such as your computer's Internet Protocol
+              ("IP") address, browser type, browser version, the pages of our
+              Site that you visit, the time and date of your visit, the time
+              spent on those pages and other statistics. In addition, we may use
+              third party services such as Google Analytics that collect,
+              monitor and analyze this … The Log Data section is for businesses
+              that use analytics or tracking services in websites or apps, like
+              Google Analytics. For the full disclosure section, create your own
+              Privacy Policy.
+            </p>
+            <h2>Communications</h2>
+            <p>
+              We may use your Personal Information to contact you with
+              newsletters, marketing or promotional materials and other
+              information.
+            </p>
+            <h3>Cookies</h3>
+            <p>
+              Cookies are files with small amount of data, which may include an
+              anonymous unique identifier. Cookies are sent to your browser from
+              a web site and stored on your computer's hard drive. Like many
+              sites, we use "cookies" to collect information. You can instruct
+              your browser to refuse all cookies or to indicate when a cookie is
+              being sent. However, if you do not accept cookies, you may not be
+              able to use some portions of our Site.
+            </p>
+            <h2>Security</h2>
+            <p>
+              The security of your Personal Information is important to us, but
+              remember that no method of transmission over the Internet, or
+              method of electronic storage, is 100% secure. While we strive to
+              use commercially acceptable means to protect your Personal
+              Information, we cannot guarantee its absolute security.
+            </p>
+            <h2>Changes To our policy</h2>
+            <p>
+              This privacy policy is effective as of 20 December 2020 and will
+              remain in effect except with respect to any changes in its
+              provisions in the future, which will be in effect immediately
+              after being posted on this page. We reserve the right to update or
+              change our privacy policy at any time and you should check this
+              Privacy Policy periodically. Your continued use of the Service
+              after we post any modifications to the privacy policy on this page
+              will constitute your acknowledgment of the modifications and your
+              consent to abide and be bound by the modified privacy policy. If
+              we make any material changes to this privacy policy, we will
+              notify you either through the email address you have provided us,
+              or by placing a prominent notice on our website. Contact us If you
+              have any questions about this privacy policy, please contact us.
+            </p>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      </div>
+      <footer class="bg-white" aria-labelledby="footerHeading">
+        <h2 id="footerHeading" class="sr-only">
+          Footer
+        </h2>
+        <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+          <div class="mt-12 pt-8">
+            <p class="text-base text-gray-400 xl:text-center">
+              &copy; 2021 Unboxed, Inc. All rights reserved.
+            </p>
+          </div>
+        </div>
       </footer>
-    </div>
-  )
+    </>
+  );
 }
